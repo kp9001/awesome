a96ae0390f86e129abf66c5e89a1cccd20bd8991
kplante <kplante@tuta.io>
2020-11-24 03:48:42 -0500

    typo

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -40,6 +40,6 @@ echo "$options"
 
 ## This is some nonsense I wrote. It's not important at all. 
 ## But if you want to use it, it is included and will run if you install python3 and pynput 
-##(or just python and rewrite the command).
+## (or just python and rewrite the command).
 [ $options = "stop" ] && python3 ~/.config/awesome/scripts/stop.py 
 
